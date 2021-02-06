1146197d9f6ba3753109be471445f32673956c7b
Edilmo Palencia <edilmo@gmail.com>
2021-02-05 18:11:55 -0800

    Consume wheels and not git links

--- setup.py
+++ setup.py
@@ -11,8 +11,13 @@ setup(
                       'h5py', 
                       'termcolor', # adept_envs dependency
                       'click',  # adept_envs dependency
-                      'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
-                      'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
+                      # These dependencies cannot be consumed from source 
+                      # because ADO is not allowing direct
+                      # links in Wheels for security reasons.
+                      # For MJRL we created custom Wheels as well.
+                      'dm_control', # @ git+git://github.com/deepmind/dm_control@master#egg=dm_control
+                      'mjrl', # @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'
+                      ],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
                            'hand_manipulation_suite/assets/*',
