780a3108839144f9a05522ccc9f3038c3a30c784
Edilmo Palencia <edilmo@gmail.com>
2021-02-05 18:14:37 -0800

    Minimal CI (#1)
    * Minimal CI
    
    * Upload verbose
    
    * Consume wheels and not git links

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='d4rl',
-    version='1.1',
+    version='1.1.0.dev0',
     install_requires=['gym', 
                       'numpy', 
                       'mujoco_py', 
@@ -11,8 +11,13 @@ setup(
                       'h5py', 
                       'termcolor', # adept_envs dependency
                       'click',  # adept_envs dependency
-                      'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
-                      'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
+                      # These dependencies cannot be consumed from source 
+                      # because ADO is not allowing direct
+                      # links in Wheels for security reasons.
+                      # For MJRL we created custom Wheels as well.
+                      'dm_control', # @ git+git://github.com/deepmind/dm_control@master#egg=dm_control
+                      'mjrl', # @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'
+                      ],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
                            'hand_manipulation_suite/assets/*',
