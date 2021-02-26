a3fc9d21c17a9e47bf15fca2b28ba4abcbae045b
Edilmo Palencia <edilmo@gmail.com>
2021-02-26 13:10:35 -0800

    Make MuJoCo optional

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     version='1.1.0.dev0',
     install_requires=['gym', 
                       'numpy', 
-                      'mujoco_py', 
                       'pybullet',
                       'h5py', 
                       'termcolor', # adept_envs dependency
